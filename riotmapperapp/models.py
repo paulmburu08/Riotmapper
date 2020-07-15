3e8d2dfca59be57f81759ca09118a4b225d29537
Lorna Kamau <kamaulorna@gmail.com>
2020-07-15 14:44:13 +0300

    add profile class and methods

--- riotmapperapp/models.py
+++ riotmapperapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_pic = CloudinaryField('Profile Picture')
+    home = models.CharField(max_length = 40, blank=True)
+    school = models.CharField(max_length = 40, blank=True)
+    work = models.CharField(max_length = 40, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete() 
+
+    def edit_home(self, new_home):
+        self.home = new_home
+        self.save()
+
+    def edit_school(self, new_school):
+        self.school = new_school
+        self.save()
+
+    def edit_work(self, new_work):
+        self.work = new_work
+        self.save()
