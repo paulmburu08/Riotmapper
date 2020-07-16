cf9b81e6ea4cfebe49b8bd83ffead0bc54aa805e
Lorna Kamau <kamaulorna@gmail.com>
2020-07-16 09:56:32 +0300

    test location and riot models

--- riotmapperapp/tests.py
+++ riotmapperapp/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Location,Riot
 
 class ProfileTestClass(TestCase):
     def setUp(self):
@@ -28,5 +28,33 @@ class ProfileTestClass(TestCase):
     def test_edit_school(self):
         self.profile.edit_school('Havard')
         self.assertEqual(self.profile.school, 'Havard')
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.nairobi = Location(name='Nairobi', lat= 1.2921, lng= 36.8219)
+        self.nairobi.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi, Location))
+
+class RiotTestClass(TestCase):
+    def setUp(self):
+        self.nairobi = Location(name='Nairobi', lat= 1.2921, lng= 36.8219)
+        self.riot = Riot(description = "Teenagers heckling and destroying property", location= self.nairobi)
+        self.nairobi.save_location()
+        self.riot.save_riot()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.riot, Riot))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+        Riot.objects.all().delete()
+    
+    def test_get_location_riots(self):
+        riots = Riot.get_location_riots(self.nairobi)
+        self.assertEqual(riots[0].description, 'Teenagers heckling and destroying property')
+        self.assertTrue(len(riots) > 0)
+    
 
 
