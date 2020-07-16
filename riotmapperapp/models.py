4569ebf635f7029310dd0c667b25f45fb9bb5634
Lorna Kamau <kamaulorna@gmail.com>
2020-07-16 09:09:41 +0300

    add riot and location classes

--- riotmapperapp/models.py
+++ riotmapperapp/models.py
@@ -29,3 +29,38 @@ class Profile(models.Model):
     def edit_work(self, new_work):
         self.work = new_work
         self.save()
+
+class Location(models.Model):
+    name = models.CharField(max_length = 40)
+    lat = models.IntegerField()
+    lng = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+class Riot(models.Model):
+    description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='location')
+
+    def __str__(self):
+        return self.description
+
+    def save_riot(self):
+        self.save()
+
+    def delete_riot(self):
+        self.delete()
+
+    @classmethod
+    def get_location_riots(cls,location):
+        return cls.objects.filter(location = location)
+
+    class Meta:
+        ordering = ['-post_date']
